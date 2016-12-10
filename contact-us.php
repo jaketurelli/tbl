6c670afece4a141777395c662e80e8575b38700c
jaketurelli <jake.turelli@gmail.com>
2016-12-10 13:01:33 -0800

    updated forgot pw modal, updated contact style

--- contact-us.php
+++ contact-us.php
@@ -10,30 +10,39 @@ include('header_content.html');
 ?>
 </head>
 <body>
-	<nav class="navbar nonhome">
+	<nav class="navbar nonhome nav-border-bottom">
 		<div class="navbar-header">
 			<a class="navbar-toggle" data-toggle="overlay" data-target=".navbar-collapse" href="#">
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</a>
-			<a class="navbar-brand navbar-brand-centered" href="index.php">The Bach League</a>
+			<a class="navbar-brand navbar-brand-centered" href="index.php"><img src="img/logo.png" alt="brand-image" /></a>
 			<ul class="nav pull-right" style="text-align:right">
-				<li style="display:none" class="loginnav"><a data-toggle="modal" data-target="#loginmodal" href="#">login</a></li>
+				<?php 
+				if($IS_SIGNED_IN){
+				?>
+					<li style="display:none" class="loginnav"><a class="logout" href="logout.php">Logout</a></li>
+				<?php
+				}else{
+				?>
+					<li style="display:none" class="loginnav"><a data-toggle="modal" data-target="#loginmodal" href="#">Login</a></li>
+				<?php
+				}
+				?>
 			</ul>
 		</div>
-		<div class=" navbar-collapse overlay">
+		<div class=" navbar-collapse overlay navbar-right">
 			<ul class="nav navbar-nav nav-pills">
-				<li><a href="index.php">home</a></li>
-				<li><a href="league.php">league</a></li>
-				<li><a href="lineup.php">lineup</a></li>
-				<li><a href="trashtalk.php">trash talk</a></li>
-				<li><a href="contestants.php">contestants</a></li>
-				<li><a href="blog.php">blog</a></li>
+				<li><a href="league.php">LEAGUE</a></li>
+				<li><a href="lineup.php">LINEUP</a></li>
+				<li><a href="trashtalk.php">TRASH TALK</a></li>
+				<li><a href="contestants.php">CONTESTANTS</a></li>
+				<li><a href="blog.php">BLOG</a></li>
 				<?php
 				if($IS_ADMIN){
 				?>
-					<li><a href="admin.php">admin</a></li>
+					<li><a href="admin.php">Admin</a></li>
 				<?php
 				}
 				?>
@@ -42,12 +51,12 @@ include('header_content.html');
 				<?php 
 				if($IS_SIGNED_IN){
 				?>
-					<li><a class="logout" href="logout.php">logout</a></li>
+					<li><a class="logout" href="logout.php">Logout</a></li>
 				<?php
 				}else{
 				?>
-					<li style="border-right: 1px solid rgba(255,255,255,0.5); text-align: right"><a class="signup"  data-toggle="modal" data-target="#signupmodal" href="#">sign up</a></li>
-					<li style="text-align:right!important"><a class="login" data-toggle="modal" data-target="#loginmodal" href="#">login</a></li>
+					<li style="border-right: 1px solid rgba(255,255,255,0.5); text-align: right"><a class="signup"  data-toggle="modal" data-target="#signupmodal" href="#">Sign up</a></li>
+					<li style="text-align:right!important"><a class="login" data-toggle="modal" data-target="#loginmodal" href="#">Login</a></li>
 				<?php
 				}
 				?>
@@ -59,13 +68,13 @@ include('header_content.html');
 			<div class="row">
 				<div class="col-md-3"></div>
 				<div class="col-md-6 col-xs-12">
-					<h3>contact us</h3>
+					<h3>Contact us</h3>
 					<form method="post" action="contact-us-send.php">
-						<label>name</label><br>
+						<label>Name</label><br>
 						<input id="name" name="name"><br>
-						<label>email</label><br>
+						<label>Email</label><br>
 						<input id="email" name="email" type="email"><br>
-						<label>message</label><br>
+						<label>Message</label><br>
 						<textarea id="message" rows="8" class="message" name="message" type="text"></textarea>
 						<input id="submit" class="btn pull-right text-center" name="submit" type="submit" value="SUBMIT">
 					</form>
