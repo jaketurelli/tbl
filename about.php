e1f3636ef9a39f0fb9361ec182a16d9f11c1948c
Kacy McKibben <kacy.mckibben@gmail.com>
2016-12-27 17:10:04 -0800

    updated about

--- about.php
+++ about.php
@@ -84,9 +84,9 @@ include('header_content.html');
 			<div class="row">
 				<div class="col-md-3"></div>
 				<div class="col-md-6 col-xs-12">
-					<h3>about the bach league</h3>
+					<h3>About The Bach League</h3>
 					<p>Hey y'all! Thank you for visiting our site, The Bach League! We've created this site so that friends can play a fun game around the TV shows, "The Bachelor" and "The Bachelorette". The site is currently in a beta version which means we've implemented the minimum amount of features/functionality that the site needs. We're currently working on the next version which will be the first full-featured version of the site. Stay tuned!</p>
-					<h3>about the creators</h3>
+					<h3>About the Creators</h3>
 					<p>Our names are Jake and Kacy. We're budding web developers/designers in beautiful Los Angeles, California. We hope you enjoy the site!</p>
 				</div>
 				<div class="col-md-3"></div>
