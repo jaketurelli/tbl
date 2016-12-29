ca531e06ea7a2ea1fc65b921f2874ba8981aaab1
jaketurelli <jake.turelli@gmail.com>
2016-12-28 17:56:19 -0800

    minor updates

--- commissioner_invite_user.php
+++ commissioner_invite_user.php
@@ -10,7 +10,8 @@ if (isset($_POST['invite'])) {
 	$headers[] = 'MIME-Version: 1.0';
 	$headers[] = 'Content-type: text/html; charset=iso-8859-1';
 	$subject = 'You Have Been Invited to Join theBachLeague';
-	$league_invite_code = rand()*rand();
+	$league_invite_code = rand(100000, 999999);
+	$league_code_string = (string)$league_invite_code;
     $query = "INSERT INTO `league_join_code` (`id`, `user_email`, `league_id` , `code` ) VALUES (NULL, '$user_email', $LEAGUE_ID, $league_invite_code)";
     $add_user_code = mysqli_query($dbc,$query) or die ("Error in query: $query " . mysqli_error($dbc));
 
@@ -19,7 +20,7 @@ if (isset($_POST['invite'])) {
 	$message_html .= '  <body>';
 	$message_html .= '    <p>You have been invited to join <b>'. $LEAGUE_NAME. '</b> by the commissioner, <b>' . $COMMISSIONER. '</b>.<p>';
 	$message_html .= '    <p>Please use the following League Code to join:<br>';
-	$message_html .=        'League Code: ' . '<b>' . strval($league_invite_code) . '<b></p>';
+	$message_html .=        'League Code: <b>' . $league_code_string . '</b></p>';
 	$message_html .= '  </body>';
 	$message_html .= '</html>';
 
@@ -29,7 +30,7 @@ if (isset($_POST['invite'])) {
 }
 
 // RELOCATE TO COMMISSIONER PAGE
-$_SESSION['COMMISSIONER_TAB'] = 1; // league email is the first tab
+$_SESSION['COMMISSIONER_TAB'] = 5; // invite user is fifth tab
 echo "<script>alert('Email sent!');</script>";
 echo "<script>window.location.href='commissioner.php';</script>";
 
