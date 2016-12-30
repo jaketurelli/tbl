ea016058d8fe150edbb7c47650a6b2ce722f7af0
Kacy McKibben <kacy.mckibben@gmail.com>
2016-12-29 19:25:08 -0800

    create join

--- createjoin.php
+++ createjoin.php
@@ -43,14 +43,14 @@
 			<div class="row">
 				<div class="col-md-3"></div>
 				<div class="col-md-6">
-					<div id="accordion" class="createjoin-panel panel-group">
+					<div class="createjoin-panel panel-group">
 					    <div class="panel panel-default">
-					        <div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#collapseOne">
+					        <div class="panel-heading">
 					            <h4 class="panel-title">
 					                <a href="#">Create a New League</a>
 					            </h4>
 					        </div>
-					        <div id="collapseOne" class="panel-collapse collapse">
+					        <div>
 					            <div class="panel-body">
 									<form action="createleague.php" method="post">
 										<fieldset>
@@ -72,11 +72,11 @@
 					    </div>
 					    <div class="panel panel-default">
 					        <div class="panel-heading">
-					            <h4 class="panel-title" data-toggle="collapse" data-parent="#accordion" data-target="#collapseTwo">
+					            <h4 class="panel-title">
 					                <a href="#">Join an Existing League</a>
 					            </h4>
 					        </div>
-					        <div id="collapseTwo" class="panel-collapse collapse">
+					        <div>
 					            <div class="panel-body">
 					            	<form action="joinleague.php" method="post">
 										<fieldset>
@@ -89,7 +89,7 @@
 											<input type="password" class="form-control" name="leaguepassword" value="" placeholder = "Enter league password here"/>
 										</div>
 										<br>
-										<div>OR:</div>
+										<div class="text-center">OR:</div>
 										<div class = "tile">
 											<label>League Code:</label>
 											<input type="text" class="form-control" name="leaguecode" value="" placeholder = "Enter emailed code here"/>
